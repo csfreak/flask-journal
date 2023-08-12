2d902859d0fad1ec9be9701a74cd5a22ea4fc395
Jason Ross <jaross@redhat.com>
2023-08-12 12:17:08 -0500

    migrate smoke tests

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -1,18 +1,18 @@
-import typing as t
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-class TestSmoke:
-    def test_app_instance(self: t.Self, app: Flask) -> None:
-        assert isinstance(app, Flask)
+def test_app_instance(app: Flask) -> None:
+    assert isinstance(app, Flask)
 
-    def test_app_config(self: t.Self, app: Flask) -> None:
-        assert app.config["TESTING"]
 
-    def test_db_type(self: t.Self, db: SQLAlchemy) -> None:
-        assert isinstance(db, SQLAlchemy)
+def test_app_config(app: Flask) -> None:
+    assert app.config["TESTING"]
 
-    def test_db_instance(self: t.Self, app: Flask, db: SQLAlchemy) -> None:
-        assert db is app.extensions["sqlalchemy"]
+
+def test_db_type(db: SQLAlchemy) -> None:
+    assert isinstance(db, SQLAlchemy)
+
+
+def test_db_instance(app: Flask, db: SQLAlchemy) -> None:
+    assert db is app.extensions["sqlalchemy"]
