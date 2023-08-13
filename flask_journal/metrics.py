91e00c4a9dbcb6f9ca29ceb898dc6eb50319fee2
Jason Ross <jaross@redhat.com>
2023-08-12 23:13:01 -0500

    ci(build): replace build

--- flask_journal/metrics.py
+++ flask_journal/metrics.py
@@ -1,7 +1,7 @@
-import os
-
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
+from . import __version__ as APP_VERSION
 
 metrics = GunicornPrometheusMetrics.for_app_factory()
 # static information as metric
-metrics.info("app_info", "Application info", version=os.getenv("APP_VERSION"))
+metrics.info("app_info", "Application info", version=APP_VERSION)
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,13 +2,16 @@
 cd /app
 source .venv/bin/activate
 
-if [ $# -eq 0 ];  then
+if [ $# -ne 0 ];  then
     exec $@
 fi
 
 GUNICORN_CMD_ARGS="--chdir /app  --threads ${WORKER_THREADS}  --config gunicorn.config.py ${EXTRA_ARGS}"
 
 PROMETHEUS_MULTIPROC_DIR=${PROMETHEUS_MULTIPROC_DIR:-/prom}
+if [ -e ${PROMETHEUS_MULTIPROC_DIR} ]; then
+    rm -rf ${PROMETHEUS_MULTIPROC_DIR}
+fi
 mkdir -p ${PROMETHEUS_MULTIPROC_DIR}
 export PROMETHEUS_MULTIPROC_DIR
 
@@ -19,6 +22,5 @@ if [ ${DEBUG} ]; then
     export FLASK_JOURNAL_DEBUG=1
 fi
 
-flask db upgrade ${ALEMBIC_REVISION}
 export GUNICORN_CMD_ARGS
 exec gunicorn 'flask_journal.app:create_app()'
