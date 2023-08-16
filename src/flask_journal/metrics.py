1c663bbfba94d8751c9a16b3b5955d414bca1167
Jason Ross <jaross@redhat.com>
2023-08-16 14:02:11 -0500

    feat(metrics): change metrics to internal server.

--- src/flask_journal/metrics.py
+++ src/flask_journal/metrics.py
@@ -1,7 +1,26 @@
-from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+import logging
+import os
+
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 from . import __version__ as APP_VERSION
 
-metrics = GunicornPrometheusMetrics.for_app_factory()
-# static information as metric
-metrics.info("app_info", "Application info", version=APP_VERSION)
+logger = logging.getLogger(__name__)
+
+metrics: PrometheusMetrics
+
+if os.getenv("IS_GUNICORN"):
+    metrics = GunicornInternalPrometheusMetrics.for_app_factory()
+else:
+    metrics = PrometheusMetrics.for_app_factory()
+
+try:
+    metrics.info("app_info", "Application info", version=APP_VERSION)
+except ValueError:
+    logger.debug("Skipping app_info metric duplicate")
+
+
+def init_metrics(app: Flask) -> None:
+    metrics.init_app(app)
