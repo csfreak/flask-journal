f77525b8bc6a38d612600805b9834665828bac60
Jason Ross <jaross@redhat.com>
2023-08-16 14:44:57 -0500

    test(metrics): add tests of /metrics endpoint

--- src/flask_journal/__init__.py
+++ src/flask_journal/__init__.py
@@ -1,7 +1,9 @@
+import logging
 from importlib.metadata import PackageNotFoundError, version
+
+logger = logging.getLogger(__name__)
 
 try:
     __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
+except PackageNotFoundError:  # pragma: no cover
+    logger.debug("Unable to read version, not installed as package")
