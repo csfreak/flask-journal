54c9f4a5ba959fe6c234fc11b1d719664d2be337
Jason Ross <github@csfreak.com>
2023-11-06 16:06:25 -0600

    update test bootstrap version

--- test/config.py
+++ test/config.py
@@ -155,11 +155,11 @@ html_test_strings = {
         "css": {
             "default": (
                 '<link rel="stylesheet"'
-                ' href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">'  # noqa: B950
+                ' href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">'  # noqa: B950
             ),
             "bootswatch": (
                 '<link rel="stylesheet"'
-                ' href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/%s/bootstrap.min.css">'  # noqa: B950
+                ' href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.2/dist/%s/bootstrap.min.css">'  # noqa: B950
             ),
         },
     },
