a24ea0b4c9834a8ca9d52e542c6f341cf43c0756
Jason Ross <jaross@redhat.com>
2023-12-07 13:38:59 -0600

    refactor(db): Use full 2.x Declarative Mapped Classes

--- src/flask_journal/models/rbac.py
+++ src/flask_journal/models/rbac.py
@@ -1,18 +1,24 @@
+import typing as t
+
 from flask_security.core import RoleMixin
-from sqlalchemy import ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .db import db
 
 
 class Role(db.Model, RoleMixin):
-    name: Mapped[str] = mapped_column(String(80), nullable=False)
-    description: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(80))
+    description: Mapped[t.Optional[str]] = mapped_column(String(255))
+
+    @classmethod
+    def find_by_name(cls: t.Self, value: str) -> t.Self:
+        return cls.find_by_attr(cls.name, value)
 
 
 class RolesUsers(db.Model):
     __tablename__ = "roles_users"
     __table_args__ = (UniqueConstraint("user_id", "role_id"),)
 
-    user_id: Mapped[int] = mapped_column("user_id", Integer(), ForeignKey("user.id"))
-    role_id: Mapped[int] = mapped_column("role_id", Integer(), ForeignKey("role.id"))
+    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("user.id"))
+    role_id: Mapped[int] = mapped_column("role_id", ForeignKey("role.id"))
